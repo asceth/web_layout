d190a0d9ee64dd184536f70e19e5e1bc8ee6e46b
asceth <machinist@asceth.com>
2009-01-03 17:08:00 -0500

    major refactor for herml

--- src/web_layout.erl
+++ src/web_layout.erl
@@ -18,11 +18,11 @@
          terminate/2, code_change/3]).
 
 %% External API
--export([register_header/3, register_footer/3]).
+-export([layout_view/1, register_layout/3, yield/1]).
 
 -include("logger.hrl").
 
--record(state, {}).
+-record(state, {layouts=[]}).
 
 -define(SERVER, ?MODULE).
 
@@ -40,19 +40,38 @@ start_link() ->
 %% External API
 %%====================================================================
 %%--------------------------------------------------------------------
-%% Function: load_pages(WebPagesName, Directory) -> ok | {error,Error}
-%% Description: Loads pages in directory, compiles them and registers
-%%               routes to them.
+%% Function: register_layout(Name, WebRouter, File) -> ok | {error,Error}
+%% Description: Registers a layout in herml with specified name to a web router.
 %%--------------------------------------------------------------------
-register_header(Prefix, WebRouter, File) when is_atom(Prefix) ->
-  gen_server:cast(?SERVER, {register_header, atom_to_list(Prefix), WebRouter, File});
-register_header(Prefix, WebRouter, File) ->
-  gen_server:cast(?SERVER, {register_header, Prefix, WebRouter, File}).
+register_layout(Name, WebRouter, File) when is_atom(Name) ->
+  gen_server:cast(?SERVER, {register_layout, atom_to_list(Name), WebRouter, File});
+register_layout(Name, WebRouter, File) ->
+  gen_server:cast(?SERVER, {register_layout, Name, WebRouter, File}).
 
-register_footer(Prefix, WebRouter, File) when is_atom(Prefix) ->
-  gen_server:cast(?SERVER, {register_footer, atom_to_list(Prefix), WebRouter, File});
-register_footer(Prefix, WebRouter, File) ->
-  gen_server:cast(?SERVER, {register_footer, Prefix, WebRouter, File}).
+%%--------------------------------------------------------------------
+%% Function: header_view(Session) -> iolist()
+%% Description: Renders the appropriate layout header.  'default' is the default layout.
+%%--------------------------------------------------------------------
+layout_view(Session) ->
+  gen_server:call(?SERVER, {execute_layout, Session}).
+
+%%--------------------------------------------------------------------
+%% Function: yield(proplist()) -> string | binary
+%% Description: looks up YieldedContent from the property list and displays it
+%%--------------------------------------------------------------------
+yield(Env) ->
+  case lists:keysearch("Session", 1, Env) of
+    false ->
+      <<"">>;
+    {value, {_Key, Value}} ->
+      case Value:flash_lookup("YieldedContent") of
+        {error, 404} ->
+          <<"">>;
+        YieldedContent ->
+          YieldedContent
+      end
+  end.
+
 
 %%====================================================================
 %% gen_server callbacks
@@ -77,6 +96,9 @@ init([]) ->
 %%                                      {stop, Reason, State}
 %% Description: Handling call messages
 %%--------------------------------------------------------------------
+handle_call({execute_layout, Session}, From, #state{layouts=Layouts} = State) ->
+  spawn(fun() -> execute_layout(From, Session:flash_lookup("layout"), Layouts, Session) end),
+  {noreply, State};
 handle_call(_Request, _From, State) ->
   Reply = ok,
   {reply, Reply, State}.
@@ -87,57 +109,19 @@ handle_call(_Request, _From, State) ->
 %%                                      {stop, Reason, State}
 %% Description: Handling cast messages
 %%--------------------------------------------------------------------
-handle_cast({register_header, Prefix, WebRouter, File}, State) ->
-  ?DEBUG("Loading up ~p for router ~p with prefix ~p~n~n", [File, WebRouter, Prefix]),
-  OutDir = case code:which(?MODULE) of
-             non_existing ->
-               filename:dirname(File);
-             preloaded ->
-               filename:dirname(File);
-             cover_compiled ->
-               filename:dirname(File);
-             ModulePath ->
-               filename:dirname(ModulePath)
-           end,
-  StrippedFileName = filename:rootname(filename:basename(File)),
-  ModuleName = Prefix ++ "_" ++ StrippedFileName,
-  case haml:compile(File, [{module, ModuleName},
-                           {outdir, OutDir}, report_errors,
-                           report_warnings, nowarn_unused_vars]) of
-    ok ->
-      web_router:add(WebRouter, pre_request_view, global,
-                     list_to_atom(ModuleName), render, 1);
-    {error, Reason} ->
-      ?ERROR_MSG("Haml Compile failed for ~s with: ~p~n",
-                 [File, Reason])
-  end,
-  {noreply, State};
-handle_cast({register_footer, Prefix, WebRouter, File}, State) ->
-  ?DEBUG("Loading up ~p for router ~p with prefix ~p~n~n", [File, WebRouter, Prefix]),
-  OutDir = case code:which(?MODULE) of
-             non_existing ->
-               filename:dirname(File);
-             preloaded ->
-               filename:dirname(File);
-             cover_compiled ->
-               filename:dirname(File);
-             ModulePath ->
-               filename:dirname(ModulePath)
-           end,
-  StrippedFileName = filename:rootname(filename:basename(File)),
-  ModuleName = Prefix ++ "_" ++ StrippedFileName,
-  case haml:compile(File, [{module, ModuleName},
-                           {outdir, OutDir}, report_errors,
-                           report_warnings, nowarn_unused_vars]) of
-    ok ->
-      web_router:add(WebRouter, post_request_view, global,
-                     list_to_atom(ModuleName), render, 1);
-    {error, Reason} ->
-      ?ERROR_MSG("Haml Compile failed for ~s with: ~p~n",
-                 [File, Reason])
-  end,
-  {noreply, State};
-
+handle_cast({register_layout, Name, WebRouter, File}, State) ->
+  ?DEBUG("Loading up ~p for router ~p for layout ~p~n~n", [File, WebRouter, Name]),
+  NewLayout = case herml_parser:file(File) of
+                {error, Reason} ->
+                  ?ERROR_MSG("Herml Compile failed for ~s with: ~p~n", [File, Reason]),
+                  [];
+                CompiledTemplate ->
+                  web_router:add(WebRouter, request_layout_view, global, web_layout, layout_view, 1),
+                  [{Name, CompiledTemplate}]
+              end,
+  Layouts = State#state.layouts,
+  State1 = #state{layouts=Layouts ++ NewLayout},
+  {noreply, State1};
 handle_cast(_Msg, State) ->
   ?ERROR_MSG("Did not recognize: ~p", [_Msg]),
   {noreply, State}.
@@ -171,3 +155,28 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+
+execute_layout(From, {error, 404}, Layouts, Session) ->
+  Reply = case lists:keysearch(default, 1, Layouts) of
+            false ->
+              % if no layout try to render just the YieldedContent from the session
+              case Session:flash_lookup("YieldedContent") of
+                {error, 404} ->
+                  <<"">>;
+                YieldedContent ->
+                  YieldedContent
+              end;
+            {value, {_Key, Value}} ->
+              herml_htmlizer:render(Value, [{"Session", Session}])
+          end,
+  gen_server:reply(From, Reply);
+
+execute_layout(From, Layout, Layouts, Session) ->
+  Reply = case lists:keysearch(Layout, 1, Layouts) of
+            false ->
+              execute_layout(From, {error, 404}, Layouts, Session);
+            {value, {_Key, Value}} ->
+              herml_htmlizer:render(Value, [{"Session", Session}])
+          end,
+  gen_server:reply(From, Reply).
+
